2d3862fed7e8bddcbe6abe395dc0cfeae817a834
Classroomgoogle <tharaniidaranthamizhmani@gmail.com>
2026-06-11 18:05:35 -0400

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -498,7 +498,7 @@ export const games: Game[] = [
     title: 'Phoenix Wright: Ace Attorney',
     description: '...',
     url: 'clpheonixrightaceattorny.html',
-    thumbnail: 'mia_fey.jpg',
+    thumbnail: 'Mia_Fey_Portrait.webp',
     category: 'Emulated'
   },
   {
